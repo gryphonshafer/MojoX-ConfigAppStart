1db2c1b8197ce8936c949d2aa1ecc63c95b2bdf6
Gryphon Shafer <gryphon@goldenguru.com>
2018-06-25 13:53:21 -0700

    Initial commit of things and stuff

---
+++ lib/MojoX/ConfigAppStart.pm
@@ -0,0 +1,102 @@
+package MojoX::ConfigAppStart;
+# ABSTRACT: Start a Mojolicious application with Config::App
+
+use 5.008;
+use strict;
+use warnings;
+use Carp 'croak';
+
+# VERSION
+
+BEGIN {
+    $ENV{CONFIGAPPENV} = $ENV{MOJO_MODE} || $ENV{PLACK_ENV} || 'development';
+}
+
+use Config::App;
+use Mojolicious::Commands;
+
+my $mojo_app_lib;
+
+sub import {
+    my ( $self, $this_mojo_app_lib ) = @_;
+    $mojo_app_lib = $this_mojo_app_lib || Config::App->new->get('mojo_app_lib');
+    return;
+}
+
+sub start {
+    my ( $self, $this_mojo_app_lib ) = @_;
+
+    $mojo_app_lib = $this_mojo_app_lib if ($this_mojo_app_lib);
+    $mojo_app_lib ||= Config::App->new->get('mojo_app_lib');
+
+    croak('Unable to determine the Mojolicious application control library') unless ($mojo_app_lib);
+
+    return Mojolicious::Commands->start_app($mojo_app_lib);
+}
+
+1;
+__END__
+=pod
+
+=begin :badges
+
+=for markdown
+[![Build Status](https://travis-ci.org/gryphonshafer/MojoX-ConfigAppStart.svg)](https://travis-ci.org/gryphonshafer/MojoX-ConfigAppStart)
+[![Coverage Status](https://coveralls.io/repos/gryphonshafer/MojoX-ConfigAppStart/badge.png)](https://coveralls.io/r/gryphonshafer/MojoX-ConfigAppStart)
+
+=end :badges
+
+=head1 SYNOPSIS
+
+    # look for "mojo_app_lib" value in Config::App configuration
+    use MojoX::ConfigAppStart;
+    MojoX::ConfigAppStart->start;
+
+    # provide explicit Mojolicious application control library on start call
+    use MojoX::ConfigAppStart;
+    MojoX::ConfigAppStart->start('Application::Control');
+
+    # provide explicit Mojolicious application control library on use call
+    use MojoX::ConfigAppStart 'Application::Control';
+    MojoX::ConfigAppStart->start;
+
+=head1 DESCRIPTION
+
+The goal of this module is to provide a simplified way to spin up Mojolicious
+applications based on settings from a L<Config::App> configuration system.
+
+If you have either C<MOJO_MODE> or C<PLACK_ENV> defined as enviornment
+variables, the value will be used as the L<Config::App> enviornment definition.
+If not defined at all, then "development" will be used. What this means in
+practice is that from the commandline, calling C<morbo> or C<hypnotoad>
+typically does what you want and expect.
+
+=head2 Effective Equivalence
+
+The following is effectively equivalent to using this module, except that the
+C<Application::Control> library is derived:
+
+    BEGIN {
+        $ENV{CONFIGAPPENV} = $ENV{MOJO_MODE} || $ENV{PLACK_ENV} || 'development';
+    }
+
+    use Config::App;
+    use Mojolicious::Commands;
+
+    Mojolicious::Commands->start_app('Application::Control');
+
+=head1 SEE ALSO
+
+You can look for additional information at:
+
+=for :list
+* L<GitHub|https://github.com/gryphonshafer/MojoX-ConfigAppStart>
+* L<CPAN|http://search.cpan.org/dist/MojoX-ConfigAppStart>
+* L<MetaCPAN|https://metacpan.org/pod/MojoX::ConfigAppStart>
+* L<AnnoCPAN|http://annocpan.org/dist/MojoX-ConfigAppStart>
+* L<Travis CI|https://travis-ci.org/gryphonshafer/MojoX-ConfigAppStart>
+* L<Coveralls|https://coveralls.io/r/gryphonshafer/MojoX-ConfigAppStart>
+* L<CPANTS|http://cpants.cpanauthors.org/dist/MojoX-ConfigAppStart>
+* L<CPAN Testers|http://www.cpantesters.org/distro/G/MojoX-ConfigAppStart.html>
+
+=cut
